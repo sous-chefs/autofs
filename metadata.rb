23d8a11a77a0749915b5bcc95e4e0907b44cc30e
Dan Webb <dan.m.webb@gmail.com>
2014-01-08 14:44:11 +0000

    Update metadata.rb
    Changed maintainer details

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,11 @@
-name            "autofs"
-maintainer        "ModCloth, Inc."
-maintainer_email  "sysops@modcloth.com"
-description       "Configures the autofs service"
-version           "0.1.5"
+name            'autofs'
+maintainer        'University of Derby'
+maintainer_email  'ai@derby.ac.uk'
+description       'Configures the autofs service'
+version           '0.1.5'
 
 supports "smartos"
 supports "solaris2"
 supports "debian"
 supports "rhel"
 supports "fedora"
-
