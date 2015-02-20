b858b6a4cf00467a638af74f59a529e7f6387405
782112 <d.webb@derby.ac.uk>
2015-02-20 16:27:41 +0000

    remove provides for platforms we haven't tested on, and first move to provider CB

--- metadata.rb
+++ metadata.rb
@@ -2,10 +2,12 @@ name            'autofs'
 maintainer        'University of Derby'
 maintainer_email  'ai@derby.ac.uk'
 description       'Configures the autofs service'
-version           '0.1.5'
+version           '0.2.0'
 
-supports "smartos"
-supports "solaris2"
-supports "debian"
-supports "rhel"
-supports "fedora"
+#supports 'smartos'
+#supports 'solaris2'
+#supports 'debian'
+#supports 'rhel'
+supports 'fedora'
+
+depends 'provider'
