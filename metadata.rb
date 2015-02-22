ab78704cd29feeb060931fffdc720274f35eb246
Damacus <dan.m.webb@gmail.com>
2015-02-22 17:26:47 +0000

    Remove double quotes

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email  'ai@derby.ac.uk'
 description       'Configures the autofs service'
 version           '0.1.5'
 
-supports "smartos"
-supports "solaris2"
-supports "debian"
-supports "rhel"
-supports "fedora"
+supports 'smartos'
+supports 'solaris2'
+supports 'debian'
+supports 'rhel'
+supports 'fedora'
