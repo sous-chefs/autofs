a673d6ad96827560a324fd5d363d3ca3b2e98d77
Luke Bradbury <luke.bradbury@derby.ac.uk>
2015-02-27 09:32:08 +0000

    test for direct auto.master entries

--- test/integration/nfs-test/serverspec/auto_master_spec.rb
+++ test/integration/nfs-test/serverspec/auto_master_spec.rb
@@ -5,3 +5,9 @@ set :backend, :exec
 describe file('/etc/auto.master') do
   it { should contain '/etc/auto.nfs' }
 end
+
+context 'direct auto.master entries' do
+  describe file('/etc/auto.master') do
+    it { should contain '/home /awesome_perl_script.pl nfsvers=3 --timeout=600' }
+  end
+end
