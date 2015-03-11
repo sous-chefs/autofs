bab43a558fcd68f8b070280b1edc0950a6930b73
Damacus <dan.m.webb@gmail.com>
2015-03-11 18:46:47 +0000

    Minor version bump to fix resource issue

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'autofs'
 maintainer 'University of Derby'
 maintainer_email 'ai@derby.ac.uk'
 description 'Configures the autofs service'
-version '0.2.0'
+version '0.2.1'
 
 depends 'resource'
 depends 'chef-sugar'
