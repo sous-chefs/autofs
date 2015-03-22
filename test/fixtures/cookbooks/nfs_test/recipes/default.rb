8031fe82d949b18b5e55c661931cbc686ae99cc0
Luke Bradbury <luke.bradbury@derby.ac.uk>
2015-03-22 03:38:39 +0000

    initial pass at resource library using line cookbook

--- test/fixtures/cookbooks/nfs_test/recipes/default.rb
+++ test/fixtures/cookbooks/nfs_test/recipes/default.rb
@@ -1,22 +1,14 @@
-autofs_nfs 'bucket' do
-  mount_point '/bucket'
-  server 'bignfs-server.home'
-  export '/example/remote_path/'
-  mount_options '-fstype=nfs4'
+automaster_entry '/home', '/awesome_perl_script.pl' do
+  options 'nfsvers=3 --timeout=600' 
 end
 
-node.default['autofs']['auto_master_entries'] = ['/home /awesome_perl_script.pl nfsvers=3 --timeout=600']
-include_recipe 'autofs'
-
-autofs_nfs 'bowl' do
-  mount_point '/bowl'
-  server 'tinynfs-server.home'
-  export '/example/remote_path/'
-  mount_options '-fstype=nfs3'
+map_entry 'userhome' do
+  location 'smbserver:export'
+  options '-fstype=smb'
+  map '/etc/auto.smb'
 end
 
-autofs_nfs '/churn' do
-  server 'tinynfs-server.home'
-  export '/example/remote_path/'
-  mount_options '-fstype=nfs3'
+nfs '/nfs/userhome' do
+  server 'nfsserver'
+  export 'export'
 end
