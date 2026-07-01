18ab1b143fa903ea62e6d2460a1a0789238383b9
Dan Webb <dan.webb@damacus.io>
2026-07-01 13:03:01 +0100

    fix: stabilize policyfile integration

--- Policyfile.rb
+++ Policyfile.rb
@@ -11,5 +11,5 @@ cookbook 'test', path: './test/cookbooks/test'
 Dir.children('./test/cookbooks/test/recipes').grep(/\.rb\z/).sort.each do |recipe|
   recipe_name = File.basename(recipe, '.rb')
 
-  named_run_list recipe_name.to_sym, 'test::' + recipe_name
+  named_run_list recipe_name.to_sym, 'recipe[test::kitchen_sudoers]', 'recipe[test::' + recipe_name + ']'
 end
